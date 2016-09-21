b76766f4fca2fe964a8a1a9fd43b7aa228a726fb
Nick White <nick41496@gmail.com>
2016-09-21 17:15:37 -0400

    Collapse nav on mobile

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,3 +1,9 @@
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
+
 $('nav a').on("click", function() { 
 	$('nav').find('.active').removeClass('active'); 
 	$(this).parent().addClass('active');
