a4f2680993f466aaef47b8dd88ff5c4b40789edf
Nick White <nick41496@gmail.com>
2016-09-21 16:51:01 -0400

    Collapse navbar on mobile when an item is selected

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,4 +1,5 @@
 $('nav a').on("click", function() { 
 	$('nav').find('.active').removeClass('active'); 
-	$(this).parent().addClass('active'); 
+	$(this).parent().addClass('active');
+	$('nav').find('.in').removeClass('in');
 });
